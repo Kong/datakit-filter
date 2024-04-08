a37991ab02705a5b1f048a7f5320065883f1b350
Hisham Muhammad <hisham@gobolinux.org>
2024-04-08 12:55:07 -0300

    feat(data): first_input_for shortcut function

--- datakit-filter/src/data.rs
+++ datakit-filter/src/data.rs
@@ -65,20 +65,20 @@ impl Payload {
                                         vec.push((k, s));
                                     }
                                 }
-                            },
+                            }
 
                             // accept string values as well
                             serde_json::Value::String(s) => {
                                 vec.push((k, s));
-                            },
+                            }
 
-                            _ => {},
+                            _ => {}
                         }
                     }
                 }
 
                 vec
-            },
+            }
             _ => {
                 // TODO
                 log::debug!("NYI: converting payload into headers vector");
@@ -123,25 +123,21 @@ impl Data {
         self.states.insert(name.to_string(), state);
     }
 
-    pub fn get_inputs_for(
-        &self,
-        name: &str,
-        waiting: Option<u32>,
-    ) -> Option<Vec<Option<&Payload>>> {
+    fn can_trigger(&self, name: &str, waiting: Option<u32>) -> bool {
         // If node is Done, avoid producing inputs
         // and re-triggering its execution.
         if let Some(state) = self.states.get(name) {
             match state {
                 State::Done(_) => {
-                    return None;
+                    return false;
                 }
                 State::Waiting(w) => match &waiting {
                     Some(id) => {
                         if w != id {
-                            return None;
+                            return false;
                         }
                     }
-                    None => return None,
+                    None => return false,
                 },
                 // State::Ready() => {}
             }
@@ -153,9 +149,21 @@ impl Data {
             match val {
                 Some(State::Done(_)) => {}
                 _ => {
-                    return None;
+                    return false;
                 }
             };
+        }
+
+        true
+    }
+
+    pub fn get_inputs_for(
+        &self,
+        name: &str,
+        waiting: Option<u32>,
+    ) -> Option<Vec<Option<&Payload>>> {
+        if !self.can_trigger(name, waiting) {
+            return None;
         }
 
         // If so, allocate the vector with the result.
@@ -167,5 +175,31 @@ impl Data {
         }
 
         Some(vec)
+    }
+
+    /// If the node is triggerable, that is, it has all its required
+    /// inputs available to trigger (i.e. none of its inputs are in a
+    /// `Waiting` state), then return the payload of the first input that
+    /// is in a `Done state.
+    ///
+    /// Note that by returning an `Option<&Payload>` this makes no
+    /// distinction between the node being not triggerable or the
+    /// node being triggerable via a `Done(None)` input.
+    ///
+    /// This is not an issue because this function is intended for use
+    /// with the implicit nodes (`response_body`, etc.) which are
+    /// handled as special cases directly by the filter.
+    pub fn first_input_for(&self, name: &str, waiting: Option<u32>) -> Option<&Payload> {
+        if !self.can_trigger(name, waiting) {
+            return None;
+        }
+
+        for input in self.graph.each_input(name) {
+            if let Some(State::Done(p)) = self.states.get(input) {
+                return p.as_ref();
+            }
+        }
+
+        None
     }
 }
